3399f7ef90525b81936cfa3d603371f1d16d6a45
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-01 00:51:14 +0200

    adaption of Messageboxes in ChatScreen

--- lib/widgets/message_box.dart
+++ lib/widgets/message_box.dart
@@ -2,24 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:quicksend/client/models.dart';
 
 class MessageBox extends StatelessWidget {
-  const MessageBox({Key? key, required this.message, required this.color})
-      : super(key: key);
+  const MessageBox({Key? key, required this.message}) : super(key: key);
   final Message message;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          message.asString(),
-          style: Theme.of(context)
-              .textTheme
-              .bodyText1
-              ?.copyWith(color: Colors.black),
+    return Align(
+      alignment: message.direction == MessageDirection.outgoing
+          ? const Alignment(1.0, 1.0)
+          : const Alignment(-1.0, 1.0),
+      child: Card(
+        elevation: 2,
+        color: message.direction == MessageDirection.outgoing
+            ? Theme.of(context).primaryColor
+            : Theme.of(context).secondaryHeaderColor,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            message.asString(),
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(color: Colors.black),
+          ),
         ),
       ),
     );
