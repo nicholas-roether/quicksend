f3755ed79cce40c8dfa44e6fd9c81f942e41243e
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-03 00:46:26 +0200

    add registered devices screen

--- lib/screens/settings/registered_devices_screen.dart
+++ lib/screens/settings/registered_devices_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:quicksend/client/models.dart';
+import 'package:quicksend/client/provider.dart';
+import 'package:quicksend/widgets/loading_indicator.dart';
 
 class RegisteredDevices extends StatelessWidget {
   const RegisteredDevices({Key? key}) : super(key: key);
 
+  String getDevice(int type) {
+    switch (type) {
+      case 0:
+        return "unknown device";
+      case 1:
+        return "mobile device";
+      case 2:
+        return "desktop device";
+      case 3:
+        return "command line interface";
+    }
+    return type.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final quicksendClient = QuicksendClientProvider.get(context);
+    final registeredDevices = quicksendClient.getRegisteredDevices();
+    initializeDateFormatting();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -12,12 +35,28 @@ class RegisteredDevices extends StatelessWidget {
           style: Theme.of(context).textTheme.headline5,
         ),
       ),
-      /*body: AnimatedBuilder(
-				animation: ,
-        builder: (context, _) {
-					return ListView.builder(itemBuilder: () {}, itemCount: 3,);
-				},
-      ),*/
+      body: FutureBuilder<List<DeviceInfo>>(
+        future: registeredDevices,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LoadingIndicator();
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  "${snapshot.data![index].name} - ${getDevice(snapshot.data![index].type)}",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                subtitle: Text(
+                  "last activity: ${DateFormat('dd.MM.yyyy EEEE', 'de').format(snapshot.data![index].lastActivity)}",
+                ),
+              );
+            },
+            itemCount: snapshot.data!.length,
+          );
+        },
+      ),
     );
   }
 }
