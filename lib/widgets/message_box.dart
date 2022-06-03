735a5287040d1cec29c3d19ee483357e93867c1c
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-03 19:03:23 +0200

    little fixes with image visualization

--- lib/widgets/message_box.dart
+++ lib/widgets/message_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quicksend/client/models.dart';
+import 'package:quicksend/widgets/image_box_widget.dart';
 
 class MessageBox extends StatelessWidget {
   const MessageBox({Key? key, required this.message}) : super(key: key);
@@ -8,17 +9,6 @@ class MessageBox extends StatelessWidget {
   Widget messageBoxContent(context) {
     switch (message.type) {
       case "text/plain":
-        /*if (message.state == MessageState.sending) {
-          return SkeletonParagraph(
-            style: SkeletonParagraphStyle(
-              lines: 1,
-              lineStyle: SkeletonLineStyle(
-                width: message.asString().length.toDouble(),
-                alignment: Alignment.bottomRight,
-              ),
-            ),
-          );
-        }*/
         if (message.state == MessageState.sent) {
           return Text(
             message.asString(),
@@ -27,52 +17,59 @@ class MessageBox extends StatelessWidget {
                 .bodyText1
                 ?.copyWith(color: Colors.black),
           );
+        } else if (message.state == MessageState.sending) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                message.asString(),
+                style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                      color: Colors.black,
+                    ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: SizedBox(
+                  height: 10,
+                  width: 10,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                    strokeWidth: 1,
+                  ),
+                ),
+              ),
+            ],
+          );
         }
         break;
       case "image/jpeg":
-        return Image.memory(
-          message.content,
-          fit: BoxFit.cover,
-          filterQuality: FilterQuality.low,
-          scale: 2.0,
-          errorBuilder: (context, error, stackTrace) {
-            return const Text("Couldn't load Image!");
-          },
-        );
-      case "image/png":
-        return Image.memory(
-          message.content,
-          fit: BoxFit.cover,
-          filterQuality: FilterQuality.low,
-          scale: 2.0,
-          errorBuilder: (context, error, stackTrace) {
-            return const Text("Couldn't load Image!");
-          },
-        );
-    }
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          message.asString(),
-          style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                color: Colors.black,
-              ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 8.0),
-          child: SizedBox(
-            height: 10,
-            width: 10,
+        if (message.state == MessageState.sent) {
+          return ImageBoxWidget(byteImage: message.content);
+        } else if (message.state == MessageState.sending) {
+          return const SizedBox(
+            height: 200,
+            width: 200,
             child: CircularProgressIndicator(
               color: Colors.black,
-              strokeWidth: 1,
             ),
-          ),
-        ),
-      ],
-    );
+          );
+        }
+        break;
+      case "image/png":
+        if (message.state == MessageState.sent) {
+          return ImageBoxWidget(byteImage: message.content);
+        } else if (message.state == MessageState.sending) {
+          return const SizedBox(
+            height: 200,
+            width: 200,
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          );
+        }
+    }
+    return const SizedBox();
   }
 
   @override
