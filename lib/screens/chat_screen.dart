2ce97c236f62a81d851f64f9634211097e67d517
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-08 01:15:34 +0200

    additional fix hero animation

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -70,7 +70,7 @@ class _ChatScreenState extends State<ChatScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Hero(
-            tag: "username",
+            tag: "username" + widget.userInfo.username,
             child: Text(
               widget.userInfo.getName(),
               style: Theme.of(context).textTheme.headline5,
