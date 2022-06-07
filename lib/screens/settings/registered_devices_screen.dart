7aecaf10d7c09612b95f5340f9cc05af9253f4fb
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-08 01:48:34 +0200

    remove column

--- lib/screens/settings/registered_devices_screen.dart
+++ lib/screens/settings/registered_devices_screen.dart
@@ -36,59 +36,54 @@ class RegisteredDevices extends StatelessWidget {
           style: Theme.of(context).textTheme.headline5,
         ),
       ),
-      body: Column(
-        children: [
-          const Text("Swipe left to remove device"),
-          FutureBuilder<List<DeviceInfo>>(
-            future: registeredDevices,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return LoadingIndicator();
-              }
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(snapshot.data![index].name),
-                    confirmDismiss: (_) async {
-                      try {
-                        await quicksendClient
-                            .removeDevice(snapshot.data![index].id);
-                        return true;
-                      } on Exception catch (_) {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return const CustomErrorWidget(
-                                message:
-                                    "Cannot remove the device that is currently in use!");
-                          },
-                        );
-                        return false;
-                      }
-                    },
-                    background: Container(
-                      decoration: const BoxDecoration(color: Colors.red),
-                      child: const Icon(Icons.delete),
-                      alignment: Alignment.centerRight,
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    ),
-                    direction: DismissDirection.endToStart,
-                    child: ListTile(
-                      title: Text(
-                        "${snapshot.data![index].name} - ${getDevice(snapshot.data![index].type)}",
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      subtitle: Text(
-                        "last activity: ${DateFormat('dd.MM.yyyy EEEE', 'de').format(snapshot.data![index].lastActivity)}",
-                      ),
-                    ),
-                  );
+      body: FutureBuilder<List<DeviceInfo>>(
+        future: registeredDevices,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LoadingIndicator();
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key(snapshot.data![index].name),
+                confirmDismiss: (_) async {
+                  try {
+                    await quicksendClient
+                        .removeDevice(snapshot.data![index].id);
+                    return true;
+                  } on Exception catch (_) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return const CustomErrorWidget(
+                            message:
+                                "Cannot remove the device that is currently in use!");
+                      },
+                    );
+                    return false;
+                  }
                 },
-                itemCount: snapshot.data!.length,
+                background: Container(
+                  decoration: const BoxDecoration(color: Colors.red),
+                  child: const Icon(Icons.delete),
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                direction: DismissDirection.endToStart,
+                child: ListTile(
+                  title: Text(
+                    "${snapshot.data![index].name} - ${getDevice(snapshot.data![index].type)}",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  subtitle: Text(
+                    "last activity: ${DateFormat('dd.MM.yyyy EEEE', 'de').format(snapshot.data![index].lastActivity)}",
+                  ),
+                ),
               );
             },
-          ),
-        ],
+            itemCount: snapshot.data!.length,
+          );
+        },
       ),
     );
   }
