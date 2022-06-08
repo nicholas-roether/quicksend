876ceb0bcb19aa320df6a7fff3a583e6bd5881e6
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-08 20:20:27 +0200

    implement chat dismiss functionalities

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quicksend/client/models.dart';
+import 'package:quicksend/client/quicksend_client.dart';
 import 'package:quicksend/widgets/profile_picture.dart';
 import 'package:skeletons/skeletons.dart';
 
-import '../client/chat.dart';
 import '../screens/chat_screen.dart';
 
 class ChatTile extends StatefulWidget {
@@ -33,6 +32,7 @@ class _ChatTileState extends State<ChatTile> {
 
   @override
   Widget build(BuildContext context) {
+    final quicksendClient = QuicksendClientProvider.get(context);
     widget.chat.getRecipient().then((value) {
       if (!mounted) return;
       setState(() {
@@ -51,6 +51,82 @@ class _ChatTileState extends State<ChatTile> {
     }
     return Dismissible(
       key: Key(userInfo!.username),
+      /*confirmDismiss: (dismissDirection) async {
+        if (dismissDirection == DismissDirection.endToStart) {
+          try {
+            await quicksendClient
+                .getChatList()
+                .deleteChat(widget.chat.recipientId);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Theme.of(context).primaryColor,
+                margin: const EdgeInsets.all(5),
+                behavior: SnackBarBehavior.floating,
+                content: Text(
+                  "Deleted chat",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      ?.copyWith(color: Colors.black),
+                ),
+              ),
+            );
+            return true;
+          } on Exception catch (_) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return const CustomErrorWidget(
+                    message: "Something went wrong!");
+              },
+            );
+            return false;
+          }
+        }
+        if (dismissDirection == DismissDirection.startToEnd) {
+          try {
+            quicksendClient.getChatList().removeChat(widget.chat.recipientId);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Theme.of(context).primaryColor,
+                margin: const EdgeInsets.all(5),
+                behavior: SnackBarBehavior.floating,
+                content: Text(
+                  "Archived chat",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      ?.copyWith(color: Colors.black),
+                ),
+              ),
+            );
+            return true;
+          } on Exception catch (_) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return const CustomErrorWidget(
+                    message: "Something went wrong!");
+              },
+            );
+            return false;
+          }
+        }
+        return null;
+      },*/
+      secondaryBackground: Container(
+        decoration: const BoxDecoration(color: Colors.red),
+        child: const Icon(Icons.delete),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      ),
+      background: Container(
+        decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+        child: const Icon(Icons.archive),
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      ),
+      direction: DismissDirection.none, // dismissdirection to horizontal
       // add removeChat function
       child: ListTile(
         leading: ProfilePicture(userInfo: userInfo!),
