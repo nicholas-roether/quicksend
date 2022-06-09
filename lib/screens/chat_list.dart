2f7b3072a27f11eff044e19f1c6ab0eeae51acfc
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-09 17:11:18 +0200

    add dismissible to chatlist

--- lib/screens/chat_list.dart
+++ lib/screens/chat_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quicksend/client/quicksend_client.dart';
 import 'package:quicksend/widgets/chat_tile.dart';
+import 'package:quicksend/widgets/custom_error_alert_widget.dart';
 
 class ChatList extends StatelessWidget {
   const ChatList({Key? key}) : super(key: key);
@@ -8,24 +9,114 @@ class ChatList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final quicksendClient = QuicksendClientProvider.get(context);
-    return AnimatedBuilder(
-      animation: quicksendClient.getChatList(),
-      builder: (context, _) {
-        final chats = quicksendClient.getChatList().getChats();
-        if (chats.isEmpty) {
-          return Center(
-            child: Text(
-              'Press the "+" button to add a chat',
-              style: Theme.of(context).textTheme.headline4,
-              textAlign: TextAlign.center,
-            ),
-          );
-        }
-        return ListView.builder(
-          itemBuilder: (context, index) => ChatTile(chat: chats[index]),
-          itemCount: chats.length,
-        );
+    return RefreshIndicator(
+      onRefresh: () {
+        return quicksendClient.refreshMessages();
       },
+      child: AnimatedBuilder(
+        animation: quicksendClient.getChatList(),
+        builder: (context, _) {
+          final chats = quicksendClient.getChatList().getChats();
+          if (chats.isEmpty) {
+            return Center(
+              child: Text(
+                'Press the "+" button to add a chat',
+                style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key(chats[index].recipientId),
+                child: ChatTile(chat: chats[index]),
+                confirmDismiss: (dismissDirection) async {
+                  if (dismissDirection == DismissDirection.endToStart) {
+                    try {
+                      await quicksendClient
+                          .getChatList()
+                          .deleteChat(chats[index].recipientId);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          duration: const Duration(seconds: 2),
+                          content: Text(
+                            "Deleted chat",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                ?.copyWith(color: Colors.black),
+                          ),
+                        ),
+                      );
+                      return true;
+                    } on Exception catch (_) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const CustomErrorWidget(
+                            message: "Something went wrong!",
+                          );
+                        },
+                      );
+                      return false;
+                    }
+                  }
+                  if (dismissDirection == DismissDirection.startToEnd) {
+                    try {
+                      quicksendClient
+                          .getChatList()
+                          .removeChat(chats[index].recipientId);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          duration: const Duration(seconds: 2),
+                          content: Text(
+                            "Archived chat",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                ?.copyWith(color: Colors.black),
+                          ),
+                        ),
+                      );
+                      return true;
+                    } on Exception catch (_) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const CustomErrorWidget(
+                            message: "Something went wrong!",
+                          );
+                        },
+                      );
+                      return false;
+                    }
+                  }
+                  return null;
+                },
+                secondaryBackground: Container(
+                  decoration: const BoxDecoration(color: Colors.red),
+                  child: const Icon(Icons.delete),
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                background: Container(
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor),
+                  child: const Icon(Icons.archive),
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                direction: DismissDirection.horizontal,
+                // add removeChat function
+              );
+            },
+            itemCount: chats.length,
+          );
+        },
+      ),
     );
   }
 }
--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -32,7 +32,6 @@ class _ChatTileState extends State<ChatTile> {
 
   @override
   Widget build(BuildContext context) {
-    final quicksendClient = QuicksendClientProvider.get(context);
     widget.chat.getRecipient().then((value) {
       if (!mounted) return;
       setState(() {
@@ -49,111 +48,41 @@ class _ChatTileState extends State<ChatTile> {
         padding: const EdgeInsets.symmetric(horizontal: 15),
       );
     }
-    return Dismissible(
-      key: Key(userInfo!.username),
-      /*confirmDismiss: (dismissDirection) async {
-        if (dismissDirection == DismissDirection.endToStart) {
-          try {
-            await quicksendClient
-                .getChatList()
-                .deleteChat(widget.chat.recipientId);
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
+    return ListTile(
+      leading: ProfilePicture(userInfo: userInfo!),
+      trailing: widget.chat.hasUnreadMessages()
+          ? SizedBox(
+              height: 10,
+              width: 10,
+              child: CircleAvatar(
                 backgroundColor: Theme.of(context).primaryColor,
-                margin: const EdgeInsets.all(5),
-                behavior: SnackBarBehavior.floating,
-                content: Text(
-                  "Deleted chat",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2
-                      ?.copyWith(color: Colors.black),
-                ),
               ),
-            );
-            return true;
-          } on Exception catch (_) {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return const CustomErrorWidget(
-                    message: "Something went wrong!");
-              },
-            );
-            return false;
-          }
-        }
-        if (dismissDirection == DismissDirection.startToEnd) {
-          try {
-            quicksendClient.getChatList().removeChat(widget.chat.recipientId);
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: Theme.of(context).primaryColor,
-                margin: const EdgeInsets.all(5),
-                behavior: SnackBarBehavior.floating,
-                content: Text(
-                  "Archived chat",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2
-                      ?.copyWith(color: Colors.black),
-                ),
-              ),
-            );
-            return true;
-          } on Exception catch (_) {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return const CustomErrorWidget(
-                    message: "Something went wrong!");
-              },
-            );
-            return false;
-          }
-        }
-        return null;
-      },*/
-      secondaryBackground: Container(
-        decoration: const BoxDecoration(color: Colors.red),
-        child: const Icon(Icons.delete),
-        alignment: Alignment.centerRight,
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      ),
-      background: Container(
-        decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-        child: const Icon(Icons.archive),
-        alignment: Alignment.centerLeft,
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      ),
-      direction: DismissDirection.none, // dismissdirection to horizontal
-      // add removeChat function
-      child: ListTile(
-        leading: ProfilePicture(userInfo: userInfo!),
-        title: Hero(
-          tag: "username" + userInfo!.username,
-          child: Text(
-            userInfo!.getName(),
-            style: Theme.of(context).textTheme.headline6,
-          ),
+            )
+          : null,
+      title: Hero(
+        tag: "username" + userInfo!.username,
+        child: Text(
+          userInfo!.getName(),
+          style: Theme.of(context).textTheme.headline6,
         ),
-        subtitle: Text(
-          getLastMessage(),
-          style: Theme.of(context)
-              .textTheme
-              .bodyText1
-              ?.copyWith(color: Theme.of(context).secondaryHeaderColor),
-        ),
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return ChatScreen(
-                userInfo: userInfo!,
-                chat: widget.chat,
-              );
-            },
-          ),
+      ),
+      subtitle: Text(
+        getLastMessage(),
+        maxLines: 1,
+        style: Theme.of(context)
+            .textTheme
+            .bodyText1
+            ?.copyWith(color: Theme.of(context).secondaryHeaderColor),
+      ),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return ChatScreen(
+              userInfo: userInfo!,
+              chat: widget.chat,
+            );
+          },
         ),
       ),
     );
