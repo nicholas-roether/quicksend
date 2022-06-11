0c638093f66d9993d82a1185dabab2f0d0416843
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-12 01:47:19 +0200

    change profile picture from stateful to stateless

--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -1,37 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:quicksend/client/models.dart';
 
-class ProfilePicture extends StatefulWidget {
+class ProfilePicture extends StatelessWidget {
   const ProfilePicture({Key? key, this.radius = 20, required this.userInfo})
       : super(key: key);
   final double radius;
   final UserInfo userInfo;
 
   @override
-  State<ProfilePicture> createState() => _ProfilePictureState();
-}
-
-class _ProfilePictureState extends State<ProfilePicture> {
-  String? assetUrl;
-
-  @override
-  void initState() {
-    assetUrl = widget.userInfo.pfpUrl;
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    assetUrl = widget.userInfo.pfpUrl;
-    super.didChangeDependencies();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: widget.radius,
+      radius: radius,
       backgroundImage: const AssetImage("assets/img/profile-pic.png"),
-      foregroundImage: assetUrl != null ? NetworkImage(assetUrl!) : null,
+      foregroundImage:
+          userInfo.pfpUrl != null ? NetworkImage(userInfo.pfpUrl!) : null,
     );
   }
 }
