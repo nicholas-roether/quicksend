eeb555a99c6acf965fbebd7df08d8410f3021300
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-12 01:43:39 +0200

    hero gesture detection for ios swipe

--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:quicksend/client/models.dart';
 
-class ProfilePicture extends StatelessWidget {
+class ProfilePicture extends StatefulWidget {
   const ProfilePicture({Key? key, this.radius = 20, required this.userInfo})
       : super(key: key);
   final double radius;
   final UserInfo userInfo;
 
   @override
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
+
+class _ProfilePictureState extends State<ProfilePicture> {
+  String? assetUrl;
+
+  @override
+  void initState() {
+    assetUrl = widget.userInfo.pfpUrl;
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    assetUrl = widget.userInfo.pfpUrl;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: radius,
+      radius: widget.radius,
       backgroundImage: const AssetImage("assets/img/profile-pic.png"),
-      foregroundImage:
-          userInfo.pfpUrl != null ? NetworkImage(userInfo.pfpUrl!) : null,
+      foregroundImage: assetUrl != null ? NetworkImage(assetUrl!) : null,
     );
   }
 }
