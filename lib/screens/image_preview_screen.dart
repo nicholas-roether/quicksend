796d14bbc3eba757bb606f304761c56ae9119ca5
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-13 00:54:38 +0200

    add image preview screen

---
+++ lib/screens/image_preview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImagePreviewScreen extends StatelessWidget {
+  const ImagePreviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
