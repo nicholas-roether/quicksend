4cd7b14aeee17b927152667f960e231ecc4aa14c
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-06-15 00:58:05 +0200

    implement user profile screen

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -3,24 +3,87 @@ import 'package:quicksend/client/models.dart';
 import 'package:quicksend/widgets/profile_picture.dart';
 
 class UserProfileScreen extends StatelessWidget {
-  const UserProfileScreen({Key? key, required this.userInfo}) : super(key: key);
-  final UserInfo userInfo;
+  const UserProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final userInfo = ModalRoute.of(context)!.settings.arguments as UserInfo;
     return Scaffold(
-      appBar: AppBar(title: Text(userInfo.getName())),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          userInfo.getName(),
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            ProfilePicture(
-              radius: 70,
-              userInfo: userInfo,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: ProfilePicture(
+                  radius: 70,
+                  userInfo: userInfo,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: Text(
+                  "Account name:",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  userInfo.username,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Display name:",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  userInfo.getName(),
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Status message:",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  userInfo.status ?? "",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
