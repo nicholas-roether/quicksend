8e671117fd66a60feb9a3548b28b9c8dfc2261c7
nicholas-roether <nicholas.roether@t-online.de>
2022-06-17 07:32:53 +0200

    Fix icon loading in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -91,7 +91,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Image.asset(
-                          "img/icon_transparent.png",
+                          "assets/img/icon_transparent.png",
                           height: 200,
                         ),
                         Text(
